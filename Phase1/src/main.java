9672c3ea915d588f392444c4759acf16de18c81d
loraenia <laura-hermann92@web.de>
2013-04-10 13:43:58 +0200

    Final commit?

---
+++ Phase1/src/main.java
@@ -0,0 +1,163 @@
+import java.io.File;
+import java.math.BigInteger;
+
+import generated_laura.*;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+
+		int eingabe = -1;
+		while (eingabe != 0) {
+
+			if (eingabe == -1) {
+				System.out.println("Herzlich Willkommen! Was möchten Sie tun?");
+				System.out
+						.println("Möchten Sie sich ein Rezept anschauen, drücken Sie bitte die 1!");
+				System.out
+						.println("Möchten Sie ein Kommentar zu einem Rezept hinzufügen, drücken Sie bitte die 2!"
+								+ "\r\n");
+				eingabe = sc.nextInt();
+			}
+
+			if (eingabe == 1) {
+				ausgabe();
+				eingabe = 0;
+			}
+
+			else if (eingabe == 2) {
+				kommentar();
+				eingabe = 0;
+				// HIER MUSS DANN DIE KOMMENTARFUNKTION REIN!!!
+			}
+
+			else // Wenn ungültige EIngabe!!!
+			{
+				eingabe = -1;
+				System.out
+						.println("Die Eingabe ist ungültig! Bitte geben Sie entweder eine 1 oder 2 ein");// neustart
+			}
+		}
+	}
+
+	public static void ausgabe() throws Exception {
+
+		// Vorgaben durch Erstellung
+		JAXBContext jc = JAXBContext.newInstance("generated_laura");
+		Unmarshaller unmarshaller = jc.createUnmarshaller();
+		RezeptSammlung rs = (RezeptSammlung) unmarshaller.unmarshal(new File(
+				"src/3d.xml"));
+
+		// Allgemeine Angaben zum Rezept
+		System.out.println("Beschreibung des Kochbuches: " + "\r\n"
+				+ rs.getBeschreibung() + "\r\n");
+		for (int i = 0; i < rs.getRezept().size(); i++) {
+			if (rs.getRezept().get(i) instanceof Rezept) {
+				Rezept r = (Rezept) rs.getRezept().get(i);
+				System.out
+						.println("_______________________________________________________________________________");
+
+				// Ausgabe des n-ten Rezepts
+				System.out.println("\r\n" + "Name: " + r.getRezeptname());
+				System.out.println("Rezeptnummer: "
+						+ r.getIdRezept().toString());
+				System.out.println("Anmerkungen zum Rezept: "
+						+ r.getAnmerkungen());
+				System.out.println("Brennwert in kcal: " + r.getBrennwert());
+
+				// Ausgabe der Zutaten!
+				System.out.println("\r\n" + "Zutaten:");
+				for (int j = 0; j < r.getZutaten().getZutat().size(); j++) { // solange
+																				// bis
+																				// es
+																				// Zutaten
+																				// gibt
+					Zutat z = (Zutat) r.getZutaten().getZutat().get(j);
+					System.out.println("" + z.getContent() + ": "
+							+ z.getMenge() + " " + z.getEinheit());
+				}
+
+				// Ausgabe der Zubereitung
+				System.out.println("\r\n" + "Zubereitung:");
+				for (int k = 0; k < r.getZubereitung().getStep().size(); k++) { // solange
+																				// wie
+																				// es
+																				// steps
+																				// gibt.
+					Step s = (Step) r.getZubereitung().getStep().get(k);
+					System.out.println(s.getContent());
+				}
+
+				// Ausgabe der Kommentare
+//				System.out.println("\r\n" + "Kommentare:");
+//				if (r.getKommentar().equals(null))
+//					System.out.println("Es gibt moch keine Kommentare");
+//				else
+//					for (int l = 0; l < r.getKommentar().size(); l++) {
+//						System.out.println("Kommmentar"
+//								+ r.getKommentar().get(l).getIdKommentar()
+//								+ ": " + r.getKommentar().get(l).getContent());
+//					}
+
+			}// Beendung der If-Bedingung
+		}// Beendung der for-Schleife
+	}// Beendung der Methode
+
+	public static void kommentar() throws Exception {
+
+		JAXBContext jc = JAXBContext.newInstance("generated_laura");
+		Unmarshaller unmarshaller = jc.createUnmarshaller();
+		RezeptSammlung rs = (RezeptSammlung) unmarshaller.unmarshal(new File("src/3d.xml"));
+		Rezept r;
+		
+		Scanner sc = new Scanner(System.in);
+		int RezeptAuswahl;
+		int KommentarIndex = 0;
+		String kommentarString;
+		Kommentar kommentar = new Kommentar();
+		BigInteger kommentarIndexBigInteger;
+
+		System.out.println("Zu welchem bla: ");
+		System.out.println("Rezept und Nummer: ");
+		for (int i = 0; i < rs.getRezept().size(); i++) {
+
+			r = (Rezept) rs.getRezept().get(i);
+			System.out.println(r.getIdRezept() +" " + r.getRezeptname());
+
+		}
+
+		
+		RezeptAuswahl = sc.nextInt();
+		sc.nextLine();
+
+
+		for (int j = 0; j < rs.getRezept().get(RezeptAuswahl-1).getKommentare().getKommentar().size(); j++){
+			
+			int y = rs.getRezept().get(RezeptAuswahl-1).getKommentare().getKommentar().get(j).getIdKommentar().intValue();
+			
+			if (KommentarIndex < y) {
+				KommentarIndex = y;
+			}
+		}
+		kommentarIndexBigInteger = BigInteger.valueOf(KommentarIndex+1);
+		System.out.println("Schreiben Sie ein Kommentar: ");
+		kommentarString = sc.nextLine();
+		
+		kommentar.setContent(kommentarString);
+		kommentar.setIdKommentar(kommentarIndexBigInteger);
+		rs.getRezept().get(RezeptAuswahl-1).getKommentare().getKommentar().add(kommentar);
+
+		//Einfügen in die XML
+		Marshaller marshaller = jc.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		marshaller.marshal(rs, (new File("src/3d.xml")));
+		
+
+	}
+}
